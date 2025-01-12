a2987c42047995ab7b8f146aecdead19650e7f46
Andrei Liavon <andrey51095@gmail.com>
2025-01-12 20:16:27 +0100

    update docker-compose files

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 echo "Starting docker-compose..."
 if [ "$1" == "--dev" ];
   then echo "Starting dev version of the app..."
-       docker-compose -f docker-compose.dev.yml down && docker-compose -f docker-compose.dev.yml up -d --build
+       docker-compose -f docker-compose.dev.yml down --remove-orphans && docker-compose -f docker-compose.dev.yml up -d --build
   else docker-compose down && docker-compose up -d --build
 fi
 echo "App is running!"
