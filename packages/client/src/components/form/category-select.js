29a5e4321c3931f6b2cfb982b66a875e78bdbb0b
Andrei Liavon <andrey51095@gmail.com>
2025-01-23 16:19:41 +0100

    [client]

---
+++ packages/client/src/components/form/category-select.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import {useQuery} from '@apollo/client';
+import {Select} from 'baseui/select';
+
+import {GET_CATEGORIES} from '../../gql';
+
+const CategorySelect = ({value, onChange}) => {
+  const [options, setOptions] = useState([]);
+  const {loading, error} = useQuery(GET_CATEGORIES, {
+    nextFetchPolicy: 'cache-and-network',
+    onCompleted: data => {
+      const categories = data.getCategories || [];
+      setOptions(categories.map(category => ({
+        id: category,
+        label: category,
+      })));
+    },
+  });
+
+  return (
+    <Select
+      options={options}
+      value={value ? [
+        {
+          id: value,
+          label: value,
+        },
+      ] : []}
+      onChange={params => {
+        onChange(params?.option?.id);
+      }}
+      placeholder="Select or create a category"
+      creatable
+      isLoading={loading}
+      clearable
+      error={!!error}
+    />
+  );
+};
+
+export default CategorySelect;
