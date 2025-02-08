675619166d0d76c27de5e2eeabc02fcece7a60cc
Andrei Liavon <andrey51095@gmail.com>
2025-02-08 13:15:50 +0100

    [server] add error handling for addPurchases

--- packages/server/src/graphQL/resolvers/mutation/addPurchases.js
+++ packages/server/src/graphQL/resolvers/mutation/addPurchases.js
@@ -1,4 +1,15 @@
+const { GraphQLError } = require('graphql');
+
 module.exports = async (_, { purchases }, {schemas: {Purchase}}) => {
-  const newPurchases = await Purchase.insertMany(purchases);
-  return newPurchases;
+  try {
+    const newPurchases = await Purchase.insertMany(purchases);
+    return newPurchases;
+  } catch (error) {
+    throw new GraphQLError("Failed to add purchases. Please try again later.", {
+      extensions: {
+        code: "ADD_PURCHASES_ERROR",
+        originalError: error,
+      },
+    });
+  }
 }
