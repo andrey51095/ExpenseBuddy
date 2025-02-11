1faed9deb38569547036f3c769a24beb3b819515
Andrei Liavon <andrey51095@gmail.com>
2025-02-11 11:02:37 +0100

    [server] enhance purchaseSchema to use ref for itemId

--- packages/server/src/database/schemas/Purchase.js
+++ packages/server/src/database/schemas/Purchase.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
 const purchaseSchema = new mongoose.Schema({
-  itemId: { type: String, required: true },
+  itemId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Item",
+    required: true,
+  },
   quantity: { type: Number, required: true },
   unit: { type: String, required: true },
   price: { type: Number, required: true },
   discount: { type: Number, default: 0 },
-  date: { type: Date, required: true },
+  date: { type: Date, required: true, index: true },
   note: { type: String, default: "" },
 });
 
