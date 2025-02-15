7f000e4056b89ab6e5fe92c29add890779e7fcd4
Andrei Liavon <andrey51095@gmail.com>
2025-02-15 18:36:10 +0100

    [server] add deleteCurrencies check for CURRENCY_IN_USE cases

--- packages/server/src/graphQL/resolvers/mutation/deleteCurrencies.js
+++ packages/server/src/graphQL/resolvers/mutation/deleteCurrencies.js
@@ -1,13 +1,30 @@
 const { GraphQLError } = require("graphql");
 const ERROR_CODES = require("../../../constants/errorCodes");
 
-module.exports = async (_, { ids }, { schemas: { Currency }, logger }) => {
+module.exports = async (
+  _,
+  { ids },
+  { schemas: { Currency, FamilyIncome }, logger }
+) => {
   try {
+    const usedCount = await FamilyIncome.countDocuments({
+      currencyId: { $in: ids },
+    });
+    if (usedCount > 0) {
+      throw new GraphQLError(
+        "One or more currencies are in use and cannot be deleted.",
+        { extensions: { code: ERROR_CODES.CURRENCY_IN_USE } }
+      );
+    }
+
     await Currency.deleteMany({ _id: { $in: ids } });
     logger.info({ count: ids.length }, "Successfully deleted currencies");
     return ids;
   } catch (error) {
     logger.error({ err: error }, "Error deleting currencies");
+    if (error.extensions && error.extensions.code) {
+      throw error;
+    }
     throw new GraphQLError(
       "Failed to delete currencies. Please try again later.",
       {
